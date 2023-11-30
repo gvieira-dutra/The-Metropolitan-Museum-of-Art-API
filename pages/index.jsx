5777b8cca020e9f628af58850c2e1db3f092c781
Gleison <gleisonvieiraa@gmail.com>
2023-11-29 22:02:55 -0500

    declaration

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 /*********************************************************************************
- *  WEB422 – Assignment 5
- *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- *  No part of this assignment has been copied manually or electronically from any other source
- *  (including web sites) or distributed to other students.
+ *  WEB422 – Assignment 06
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+ *  assignment has been copied manually or electronically from any other source (including web sites) or
+ *  distributed to other students.
  *
- *  Name: Gleison Vieira Dutra Student ID: 119237220 Date: November 16, 2023
+ *  Name: ______________________ Student ID: ______________ Date: ________________
  *
+ *  Netlify App Link: https://monumental-marshmallow-d03548.netlify.app/
  *
  ********************************************************************************/
 
@@ -23,25 +24,25 @@ export default function Home() {
       <br />
       <Row>
         <Col md={6} align="justify">
-          The Metropolitan Museum of Art in New York City, colloquially &quot;the Met&quot;,[a] is the largest
-          art museum in the Americas. In 2022 it welcomed 3,208,832 visitors, ranking it the third
-          most visited U.S museum, and eighth on the list of most-visited art museums in the
-          world.[6] Its permanent collection contains over two million works,[1] divided among 17
-          curatorial departments. The main building at 1000 Fifth Avenue, along the Museum Mile on
-          the eastern edge of Central Park on Manhattan&apos;s Upper East Side, is by area one of the
-          world&apos;s largest art museums. The first portion of the approximately 2-million-square-foot
-          (190,000 m2) building was built in 1880. A much smaller second location, The Cloisters at
-          Fort Tryon Park in Upper Manhattan, contains an extensive collection of art, architecture,
-          and artifacts from medieval Europe.
+          The Metropolitan Museum of Art in New York City, colloquially &quot;the Met&quot;,[a] is
+          the largest art museum in the Americas. In 2022 it welcomed 3,208,832 visitors, ranking it
+          the third most visited U.S museum, and eighth on the list of most-visited art museums in
+          the world.[6] Its permanent collection contains over two million works,[1] divided among
+          17 curatorial departments. The main building at 1000 Fifth Avenue, along the Museum Mile
+          on the eastern edge of Central Park on Manhattan&apos;s Upper East Side, is by area one of
+          the world&apos;s largest art museums. The first portion of the approximately
+          2-million-square-foot (190,000 m2) building was built in 1880. A much smaller second
+          location, The Cloisters at Fort Tryon Park in Upper Manhattan, contains an extensive
+          collection of art, architecture, and artifacts from medieval Europe.
         </Col>
 
         <Col md={6} align="justify">
           The Metropolitan Museum of Art was founded in 1870 with its mission to bring art and art
-          education to the American people. The museum&apos;s permanent collection consists of works of
-          art from classical antiquity and ancient Egypt, paintings, and sculptures from nearly all
-          the European Old Masters, and an extensive collection of American and modern art. The Met
-          maintains extensive holdings of African, Asian, Oceanian, Byzantine, and Islamic art. The
-          museum is home to encyclopedic collections of musical instruments, costumes, and
+          education to the American people. The museum&apos;s permanent collection consists of works
+          of art from classical antiquity and ancient Egypt, paintings, and sculptures from nearly
+          all the European Old Masters, and an extensive collection of American and modern art. The
+          Met maintains extensive holdings of African, Asian, Oceanian, Byzantine, and Islamic art.
+          The museum is home to encyclopedic collections of musical instruments, costumes, and
           accessories, as well as antique weapons and armor from around the world. Several notable
           interiors, ranging from 1st-century Rome through modern American design, are installed in
           its galleries.{' '}
