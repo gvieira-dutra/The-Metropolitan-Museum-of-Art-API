c8b39dc53f7b38e5a5cd6fc8f71cf6330217e7d1
Gleison <gleisonvieiraa@gmail.com>
2023-11-30 22:32:51 -0500

    RouteGuard

--- pages/_app.js
+++ pages/_app.js
@@ -1,34 +1,38 @@
 import '@/styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Layout from './components/Layout'
+import dynamic from "next/dynamic"
 import { SWRConfig } from 'swr'
 
+const RouteGuard = dynamic(() => import("./components/RouteGuard"), {
+  ssr: false,
+})
 export default function App({ Component, pageProps }) {
-
   //return <Component {...pageProps} />
 
   return (
-    <Layout>
-    <SWRConfig value={{
-  fetcher:
-      async url => {
-        const res = await fetch(url)
+    <RouteGuard>
+      <Layout>
+        <SWRConfig
+          value={{
+            fetcher: async (url) => {
+              const res = await fetch(url)
 
-        // If the status code is not in the range 200-299,
-        // we still try to parse and throw it.
-        if (!res.ok) {
-          const error = new Error('An error occurred while fetching the data.')
-          // Attach extra info to the error object.
-          error.info = await res.json()
-          error.status = res.status
-          throw error
-        }
-        return res.json()
-      }
-    }}>
-      <Component {...pageProps}/>
-      </SWRConfig>
-    </Layout>
+              // If the status code is not in the range 200-299,
+              // we still try to parse and throw it.
+              if (!res.ok) {
+                const error = new Error('An error occurred while fetching the data.')
+                // Attach extra info to the error object.
+                error.info = await res.json()
+                error.status = res.status
+                throw error
+              }
+              return res.json()
+            }
+          }}>
+          <Component {...pageProps} />
+        </SWRConfig>
+      </Layout>
+    </RouteGuard>
   )
-
 }
