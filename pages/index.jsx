6f17a09d95d619ad3af4be7d1e71170949f58ba0
Gleison <gleisonvieiraa@gmail.com>
2023-11-30 22:35:35 -0500

    declaration

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@
  *  assignment has been copied manually or electronically from any other source (including web sites) or
  *  distributed to other students.
  *
- *  Name: ______________________ Student ID: ______________ Date: ________________
+ *  Name: Gleison Vieira Dutra Student ID: 119237220 Date: Nov 30, 2023
  *
  *  Netlify App Link: https://main--metropolitan-museum-api.netlify.app/
  *
