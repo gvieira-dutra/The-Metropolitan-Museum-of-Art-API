c760c829e4c3c659a3d646111031f30a324545e2
Gleison <gleisonvieiraa@gmail.com>
2023-11-30 22:31:15 -0500

    RouteGuard

---
+++ pages/components/RouteGuard.js
@@ -0,0 +1,45 @@
+import { useAtom } from 'jotai'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { favouritesAtom, searchHistoryAtom } from '@/store'
+import { isAuthenticated } from '@/my-app/lib/authenticate'
+import { getFavourites, getHistory } from '@/my-app/lib/userData'
+
+export default function RouteGuard(props) {
+  const PUBLIC_PATHS = ['/login', '/', '/_error', '/register']
+
+  const router = useRouter()
+  const [searchHistory, setSearchHistory] = useAtom(searchHistoryAtom)
+  const [favouritesList, setFavouritesList] = useAtom(favouritesAtom)
+  const [authorized, setAuthorized] = useState(false)
+
+  async function updateAtoms() {
+    setFavouritesList(await getFavourites())
+    setSearchHistory(await getHistory())
+  }
+
+  useEffect(() => {
+    updateAtoms()
+
+    authCheck(router.pathname)
+
+    router.events.on('routeChangeComplete', authCheck)
+
+    return () => {
+      router.events.off('routeChangeComplete', authCheck)
+    }
+  }, [])
+
+  function authCheck(url) {
+    // redirect to login page if accessing a private page and not logged in
+    const path = url.split('?')[0]
+    if (!isAuthenticated() && !PUBLIC_PATHS.includes(path)) {
+      setAuthorized(false)
+      router.push('/login')
+    } else {
+      setAuthorized(true)
+    }
+  }
+
+  return <>{props.children}</>
+}
