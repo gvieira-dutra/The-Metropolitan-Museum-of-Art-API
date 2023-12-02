4e018a3c9de30df3cdfff7bf77ace4dfa1cc921d
Gleison <gleisonvieiraa@gmail.com>
2023-12-01 20:50:14 -0500

    code improvement, comments reorganized

--- my-app/lib/userData.js
+++ my-app/lib/userData.js
@@ -1,16 +1,19 @@
-import { getToken } from './authenticate'
+import { getToken } from "./authenticate"
 let token = getToken()
 
+//PUT request to favourites/id route, it will allow us to add an artwork to
+//the favourites list
 export async function addToFavourites(id) {
-  //PUT request to favourites/id route, it will allow us to add an artwork to
-  //the favourites list
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/favourites/${id}`, {
-    method: 'PUT',
-    headers: {
-      Authorization: `JWT ${token}`,
-      'content-type': 'application/json'
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/favourites/${id}`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `JWT ${token}`,
+        "Content-type": "application/json",
+      },
     }
-  })
+  )
 
   if (res.status === 200) {
     return res.json()
@@ -18,17 +21,19 @@ export async function addToFavourites(id) {
     return []
   }
 }
-
+//DELETE request to favourites/id route, it will delete from the favourites
+//list the artwork with the corresponding id
 export async function removeFromFavourites(id) {
-  //DELETE request to favourites/id route, it will delete from the favourites
-  //list the artwork with the corresponding id
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/favourites/${id}`, {
-    method: 'DELETE',
-    headers: {
-      Authorization: `JWT ${token}`,
-      'content-type': 'application/json'
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/favourites/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `JWT ${token}`,
+        "Content-type": "application/json",
+      },
     }
-  })
+  )
 
   if (res.status === 200) {
     return res.json()
@@ -38,13 +43,13 @@ export async function removeFromFavourites(id) {
 }
 
 //GET request to favourites/ route, it will retrieve all favourites artwork
-export async function getFavourites(id) {
+export async function getFavourites() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/favourites`, {
-    method: 'GET',
+    method: "GET",
     headers: {
       Authorization: `JWT ${token}`,
-      'content-type': 'application/json'
-    }
+      "Content-type": "application/json",
+    },
   })
 
   if (res.status === 200) {
@@ -54,16 +59,15 @@ export async function getFavourites(id) {
   }
 }
 
-
-export async function addToHistory(id) {
-  //PUT request to history/id route, it will allow us to add an artwork to
+//PUT request to history/id route, it will allow us to add an artwork to
   //the favourites list
+export async function addToHistory(id) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/history/${id}`, {
-    method: 'PUT',
+    method: "PUT",
     headers: {
       Authorization: `JWT ${token}`,
-      'content-type': 'application/json'
-    }
+      "Content-type": "application/json",
+    },
   })
 
   if (res.status === 200) {
@@ -72,14 +76,15 @@ export async function addToHistory(id) {
     return []
   }
 }
-export async function removeFromHistory(id) {
+
   //DELETE request to history/id route, it will delete given item from history
+export async function removeFromHistory(id) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/history/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
     headers: {
       Authorization: `JWT ${token}`,
-      'content-type': 'application/json'
-    }
+      "Content-type": "application/json",
+    },
   })
 
   if (res.status === 200) {
@@ -89,17 +94,16 @@ export async function removeFromHistory(id) {
   }
 }
 
+//GET request to favourites/ route, it will retrieve all history artwork
 export async function getHistory() {
-  //GET request to favourites/ route, it will retrieve all history artwork
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/history`, {
-    method: 'GET',
+    method: "GET",
     headers: {
       Authorization: `JWT ${token}`,
-      'content-type': 'application/json'
-    }
+      "Content-type": "application/json",
+    },
   })
 
- 
   if (res.status === 200) {
     return res.json()
   } else {
